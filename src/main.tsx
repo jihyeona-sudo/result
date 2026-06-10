58d5fa4db65ae821e3eb2b6b9204461f61a23abd
jihyeona-sudo <jihyeona@hanmail.net>
2026-06-10 16:43:18 +0900

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import {StrictMode} from 'react';
+// @ts-ignore
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+// @ts-ignore
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
